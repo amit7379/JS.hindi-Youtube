d63fc20afb273ab6e13e43765821f6d7c20ca178
amit7379 <145749140+amit7379@users.noreply.github.com>
2024-09-29 11:10:44 +0000

    complete today

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId  = 144553
+let accountEmail = "kumar7379@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2  // Not Allowed
+
+accountEmail = "amit123.com"
+accountPassword = "78987"
+accountCity = "Deoria"
+console.log(accountId);
+
+console.table([accountId, accountEmail , accountPassword , accountCity ,accountState])
+
